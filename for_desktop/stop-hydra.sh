aa5ee0366113db30a95b7e9a247c0510a38da99d
Chris Warburton <chriswarbo@gmail.com>
2018-02-21 21:13:41 +0000

    Wait for Hydra to stop

--- for_desktop/stop-hydra.sh
+++ for_desktop/stop-hydra.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Stopping Hydra" 1>&2
-sudo systemctl stop hydra-evaluator hydra-queue-runner hydra-server
+sudo systemctl stop hydra-evaluator hydra-queue-runner hydra-server \
+                    hydra-send-stats
+
+echo "Waiting for Hydra processes to disappear"
+while pgrep -f hydra
+do
+    sleep 10
+done
