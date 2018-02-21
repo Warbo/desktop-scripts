096bc51e5d9ff9567901afa72093535a6158fab2
Chris Warburton <chriswarbo@gmail.com>
2018-02-21 21:14:10 +0000

    Start hydra-send-stats too

--- for_desktop/start-hydra.sh
+++ for_desktop/start-hydra.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Starting Hydra" 1>&2
-sudo systemctl start hydra-evaluator hydra-queue-runner hydra-server
+sudo systemctl start hydra-evaluator hydra-queue-runner hydra-server \
+                     hydra-send-stats
